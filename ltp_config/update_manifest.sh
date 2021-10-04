867d9756ef4b5a9b81406c47f09b54f1432a763a
Aniket Borkar <aniketx.borkar@intel.com>
2021-10-04 16:27:27 +0530

    Updating sed commands for some TCs in LTP manifest
    In this commit, the LTP manifest for some TCs are updated
    according to new format for SGX allowed files

--- ltp_config/update_manifest.sh
+++ ltp_config/update_manifest.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ $LTPSCENARIO == *"syscalls-new"* ]]; then
-    sed -i '76 a sgx.allowed_files.pipe202child = "file:pipe2_02_child"' $PWD/install-sgx/testcases/bin/pipe2_02.manifest
-    sed -i '76 a sgx.allowed_files.execvpchild = "file:execvp01_child"' $PWD/install-sgx/testcases/bin/execvp01.manifest
-    sed -i '76 a sgx.allowed_files.execvchild = "file:execv01_child"' $PWD/install-sgx/testcases/bin/execv01.manifest
-    sed -i '76 a sgx.allowed_files.execlpchild = "file:execlp01_child"' $PWD/install-sgx/testcases/bin/execlp01.manifest
-    sed -i '76 a sgx.allowed_files.execlchild = "file:execl01_child"' $PWD/install-sgx/testcases/bin/execl01.manifest 
+    sed -i '60 a pipe202child = "file:pipe2_02_child"' $PWD/install-sgx/testcases/bin/pipe2_02.manifest
+    sed -i '60 a execvpchild = "file:execvp01_child"' $PWD/install-sgx/testcases/bin/execvp01.manifest
+    sed -i '60 a execvchild = "file:execv01_child"' $PWD/install-sgx/testcases/bin/execv01.manifest
+    sed -i '60 a execlpchild = "file:execlp01_child"' $PWD/install-sgx/testcases/bin/execlp01.manifest
+    sed -i '60 a execlchild = "file:execl01_child"' $PWD/install-sgx/testcases/bin/execl01.manifest 
 fi
