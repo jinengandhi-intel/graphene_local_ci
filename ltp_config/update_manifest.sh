f8a7e26b6d2f894d78240f818f417553d9f3533e
Aniket Borkar <aniketx.borkar@intel.com>
2021-11-26 13:40:08 +0530

    Use gramine-test for building LTP manifests
    In this commit, gramine-test is enabled for LTP suite.

--- ltp_config/update_manifest.sh
+++ ltp_config/update_manifest.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ $LTPSCENARIO == *"syscalls-new"* ]]; then
-    sed -i '/sgx.allowed_files/ a pipe202child = "file:pipe2_02_child"' $PWD/install-sgx/testcases/bin/pipe2_02.manifest
-    sed -i '/sgx.allowed_files/ a execvpchild = "file:execvp01_child"' $PWD/install-sgx/testcases/bin/execvp01.manifest
-    sed -i '/sgx.allowed_files/ a execvchild = "file:execv01_child"' $PWD/install-sgx/testcases/bin/execv01.manifest
-    sed -i '/sgx.allowed_files/ a execlpchild = "file:execlp01_child"' $PWD/install-sgx/testcases/bin/execlp01.manifest
-    sed -i '/sgx.allowed_files/ a execlchild = "file:execl01_child"' $PWD/install-sgx/testcases/bin/execl01.manifest 
+    sed 's/allowed_files = \[/&"file:pipe2_02_child",/' $PWD/pipe2_02.manifest
+    sed 's/allowed_files = \[/&"file:execvp01_child",/' $PWD/execvp01.manifest
+    sed 's/allowed_files = \[/&"file:execv01_child",/' $PWD/execv01.manifest
+    sed 's/allowed_files = \[/&"file:execlp01_child",/' $PWD/execlp01.manifest
+    sed 's/allowed_files = \[/&"file:execl01_child",/' $PWD/execl01.manifest
 fi
