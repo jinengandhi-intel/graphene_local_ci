1d23e273cb23f7dc988a640232eebe5eb03c55a0
Aniket Borkar <aniketx.borkar@intel.com>
2022-05-11 15:44:10 +0530

    Updating TF parser
    The parser for TF is updated for different string in output format
    for BERT and RESNET.

--- tf_parser.py
+++ tf_parser.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import pytest
+import re
 path_bert = os.environ.get('bert_dir')
 path_resnet = os.environ.get('resnet_dir')
 path = [path_bert , path_resnet]
@@ -8,73 +9,57 @@ print ("Path : ", path)
 
 def result_aggregation(input_path):
     all_files = glob.glob(input_path + "/*.txt")
-    avg_throughput_native = []
-    avg_throughput_gramine_direct = []
-    avg_throughput_gramine_sgx = []
+    native_result = []
+    direct_result = []
+    sgx_result = []
     for filename in all_files:
         with open(filename, "r") as f:
             for row in f.readlines():
-                row = row.split()
                 if row:
-                    if "Throughput" in row[0]:
-                        throughput = row[1]
+                    if "Throughput" in row or "Total throughput" in row:
+                        throughput = re.findall('\d+\.\d+', row)
+                        throughput = round(float(throughput[0]),2)
                     else:
                         continue
                     if "native" in filename:
-                        avg_throughput_native.append(float(throughput))
+                        native_result.append(float(throughput))
                     elif "gramine-direct" in filename:
-                        avg_throughput_gramine_direct.append(float(throughput))
+                        direct_result.append(float(throughput))
                     elif "gramine-sgx" in filename:
-                        avg_throughput_gramine_sgx.append(float(throughput))
+                        sgx_result.append(float(throughput))
                     else:
                         print("Output file from config list not found.")
     
-    return avg_throughput_native, avg_throughput_gramine_direct, avg_throughput_gramine_sgx
+    print("\nNumber of iterations : ",len(sgx_result))
+    if len(native_result) > 0:
+        print("Throughput values in native run : ", native_result, 
+            " and Average : ",round(sum(native_result)/len(native_result),2))
+    if len(direct_result) > 0: 
+        print("Throughput values in Gramine direct run : ", direct_result,
+            " and Average : ", round(sum(direct_result)/len(direct_result),2))   
+    if len(sgx_result) > 0:    
+        print("Throughput values in Gramine SGX run : ", sgx_result,
+            " and Average : ", round(sum(sgx_result)/len(sgx_result),2)) 
+    if len(sgx_result) > 0 and len(native_result) > 0 and len(direct_result) > 0:
+        avg_sgx_throughput = sum(sgx_result)/len(sgx_result)
+        avg_native_throughput = sum(native_result)/len(native_result)
+        avg_direct_throughput = sum(direct_result)/len(direct_result)
+        print("Degradation Native/SGX : ", (avg_native_throughput/avg_sgx_throughput))
+        print("Degradation Native/Direct : ", (avg_native_throughput/avg_direct_throughput))
+        print("Degradation Direct/SGX : ", (avg_direct_throughput/avg_sgx_throughput)) 
+
+    return native_result, direct_result, sgx_result
 
 class Test_TF_Results():
     @pytest.mark.skipif(path_bert is None,
                     reason="BERT example not executed")    
     def test_bert_workload(self):
-        native_result, direct_result, sgx_result = result_aggregation(path_bert)
-        print("\nNumber of iterations for BERT workload : ",len(sgx_result))
-        if len(native_result) > 0:
-            print("Throughput values in native run : ", native_result)
-            print("Average Throughput Native: ",sum(native_result)/len(native_result))
-        if len(direct_result) > 0: 
-            print("Throughput values in Gramine direct run : ", direct_result)   
-            print("Average Throughput Gramine direct: ",sum(direct_result)/len(direct_result))
-        if len(sgx_result) > 0:    
-            print("Throughput values in Gramine SGX run : ", sgx_result) 
-            print("Average Throughput Gramine SGX: ",sum(sgx_result)/len(sgx_result))
-        if len(sgx_result) > 0 and len(native_result) > 0 and len(direct_result) > 0:
-            avg_sgx_throughput = sum(sgx_result)/len(sgx_result)
-            avg_native_throughput = sum(native_result)/len(native_result)
-            avg_direct_throughput = sum(direct_result)/len(direct_result)
-            print("Degradation Native/SGX : ", (avg_native_throughput/avg_sgx_throughput))
-            print("Degradation Native/Direct : ", (avg_native_throughput/avg_direct_throughput))
-            print("Degradation Direct/SGX : ", (avg_direct_throughput/avg_sgx_throughput))        
+        native_result, direct_result, sgx_result = result_aggregation(path_bert)            
         assert(len(native_result) is not None or len(sgx_result) is not None)
 
     @pytest.mark.skipif(path_resnet is None,
                     reason="RESNET example not executed")    
     def test_resnet_workload(self):
         native_result, direct_result, sgx_result = result_aggregation(path_resnet)
-        print("\nNumber of iterations for RESNET workload : ",len(sgx_result))
-        if len(native_result) > 0:
-            print("Throughput values in native run : ", native_result)
-            print("Average Throughput Native: ",sum(native_result)/len(native_result))
-        if len(direct_result) > 0: 
-            print("Throughput values in Gramine direct run : ", direct_result)   
-            print("Average Throughput Gramine direct: ",sum(direct_result)/len(direct_result))
-        if len(sgx_result) > 0:    
-            print("Throughput values in Gramine SGX run : ", sgx_result) 
-            print("Average Throughput Gramine SGX: ",sum(sgx_result)/len(sgx_result))
-        if len(sgx_result) > 0 and len(native_result) > 0 and len(direct_result) > 0:
-            avg_sgx_throughput = sum(sgx_result)/len(sgx_result)
-            avg_native_throughput = sum(native_result)/len(native_result)
-            avg_direct_throughput = sum(direct_result)/len(direct_result)
-            print("Degradation Native/SGX : ", (avg_native_throughput/avg_sgx_throughput))
-            print("Degradation Native/Direct : ", (avg_native_throughput/avg_direct_throughput))
-            print("Degradation Direct/SGX : ", (avg_direct_throughput/avg_sgx_throughput))
         assert(len(native_result) is not None or len(sgx_result) is not None)
     
