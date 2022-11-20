ccc4d07c8123635b867f440826c8f0af4e24b6a7
Anjali Rai <anjali.rai@intel.com>
2022-11-20 15:20:06 +0530

    Python Makefile and manifest.template is not needed now. Updating the enclave size for RHEL/CentOS

--- ci/resources/setup_workspace.sh
+++ ci/resources/setup_workspace.sh
@@ -20,10 +20,6 @@ cp -rf $WORKSPACE/utils/openvino_setup.sh CI-Examples/openvino/
 if [[ "$SGX" == 1 ]]; then
   cp -rf ~/jenkins/sandstone-50-bin CI-Examples/
   cp -f $WORKSPACE/Patch/rename_protected_file.patch .
-
-  if [[ "$base_os" == "ubuntu18.04" ]]; then
-    cp -rf $WORKSPACE/Python/* CI-Examples/python/
-  fi
 fi
 
 sed -i 's/.release  = "3.10.0"/.release  = "5.10.0"/' libos/src/sys/libos_uname.c
@@ -31,9 +27,4 @@ sed -i 's/.release  = "3.10.0"/.release  = "5.10.0"/' libos/src/sys/libos_uname.
 if [[ "$base_os" == *"centos"* ]]; then
   echo "setting up workspace for centos"
   cp -rf $WORKSPACE/utils/rust_centos_setup/* CI-Examples/rust/
-fi
-
-if [[ "$base_os" != *"ubuntu"* ]]; then
-  echo "Updating Python manifest file for centos/rhel"
-  sed -i -e '/dist\|apport/c\' CI-Examples/python/python.manifest.template
 fi
