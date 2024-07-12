adef6432d455a4e1a4712097e2c7c034e515c8d8
sreeharikax <sreex.harika.kalakatta@intel.com>
2024-07-12 15:55:45 +0530

    Add ubi9,ubi9 minimal and centos9 in gsc test

--- gsc/test/setup.sh
+++ gsc/test/setup.sh
@@ -7,8 +7,8 @@ elif [[ "$BUILD_OS" = "centos:8" ]]; then
     sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
     sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*
     echo 'proxy=http://proxy-dmz.intel.com:911' >> /etc/yum.conf && yum update -y && yum install -y python3
-elif [[ "$BUILD_OS" = "redhat/ubi8:8.8" ]]; then
+elif [[ "$BUILD_OS" = "redhat/ubi9:9.4" ]] || [[ "$BUILD_OS" = "quay.io/centos/centos:stream9" ]]; then
     yum update -y && yum install -y python3
-elif [[ "$BUILD_OS" = "redhat/ubi8-minimal:8.8" ]]; then
+elif [[ "$BUILD_OS" = "redhat/ubi9-minimal:9.4" ]]; then
     microdnf install -y python3;
 fi
