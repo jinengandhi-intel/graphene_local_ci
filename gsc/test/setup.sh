530390d886e49ded6c00981961fe4c323990e3fc
Anjali Rai <anjali.rai@intel.com>
2024-09-05 13:53:13 +0530

    Integrate openSUSE to graphene_local_ci in gsc pipeline.

--- gsc/test/setup.sh
+++ gsc/test/setup.sh
@@ -11,4 +11,6 @@ elif [[ "$BUILD_OS" = "redhat/ubi9:9.4" ]] || [[ "$BUILD_OS" = "quay.io/centos/c
     yum update -y && yum install -y python3
 elif [[ "$BUILD_OS" = "redhat/ubi9-minimal:9.4" ]]; then
     microdnf install -y python3;
+elif [[ "$BUILD_OS" = "registry.suse.com/suse/sle15:15.4" ]]; then
+    zypper -n update  && zypper -n install python3;
 fi
