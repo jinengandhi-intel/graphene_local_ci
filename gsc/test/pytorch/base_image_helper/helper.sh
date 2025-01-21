faa10e24c0644ff9d48ddd38797b6aa82c2eb685
vasanth-intel <vasanth.k.nagaraja@intel.com>
2025-01-21 13:06:04 +0530

    Updates to enable pytorch workload in GSC.
    Signed-off-by: vasanth-intel <vasanth.k.nagaraja@intel.com>

---
+++ gsc/test/pytorch/base_image_helper/helper.sh
@@ -0,0 +1,42 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# Copyright (C) 2022 Intel Corporation
+
+# exit when any command fails
+set -e
+
+CUR_DIR=$(pwd)
+MY_PATH=$(dirname "$0")
+cd $MY_PATH
+
+image_name='pytorch-encrypted'
+rm -rf examples
+git clone https://github.com/gramineproject/examples.git
+cd examples/pytorch
+git checkout $(git tag --list 'v*.*' --sort=taggerdate | tail -1)
+
+# Download and save the pre-trained model
+python3 download-pretrained-model.py
+cd ../../
+
+# Commenting below statement of generation of encryption_key as 
+# it is being checked-in the repo.
+#dd if=/dev/urandom bs=16 count=1 > encryption_key
+
+gramine-sgx-pf-crypt encrypt -w encryption_key -i examples/pytorch/input.jpg -o input.jpg
+gramine-sgx-pf-crypt encrypt -w encryption_key -i examples/pytorch/classes.txt -o classes.txt
+gramine-sgx-pf-crypt encrypt -w encryption_key -i examples/pytorch/alexnet-pretrained.pt -o \
+alexnet-pretrained.pt
+
+mv examples/pytorch/pytorchexample.py ./
+
+# Build pytorch base image
+docker rmi -f $image_name >/dev/null 2>&1
+docker build -t $image_name .
+
+rm -rf examples
+rm pytorchexample.py input.jpg classes.txt alexnet-pretrained.pt
+cd $CUR_DIR
+
+echo -e '\n\nCreated base image `'$image_name'`.'
+echo -e 'Please refer to `Intel-Confidential-Compute-for-X/workloads/pytorch/README.md` to ' \
+'generate a Gramine-protected version of this Docker image.\n'
---
+++ gsc/verifier/helper.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# Copyright (C) 2022 Intel Corporation
+
+# This script takes input from curate.py and creates verifier image.
+# The script is called only when attestation is required by the user.
+
+# The input parameters in sequence are below:
+# -- arg1    : 'done' means user provided his own certs in verifier/ssl directory or
+#              'test' means non-production ready certs will be used.
+#              The certs are finally copied to ssl_common directory for future use by GSC image and
+#              the verifier image.
+# -- arg2    : y or n (encrypted files to be used with workload?)
+# -- arg3    : Encryption key path. `CMD ["key-path"]` instruction will be appended to
+#              verifier.dockerfile when this variable is set.
+
+set -e
+echo printing args $0 $@
+
+MY_PATH=$(dirname "$0")
+pushd ${MY_PATH}
+
+docker rmi -f verifier >/dev/null 2>&1
+rm -rf  ssl_common >/dev/null 2>&1
+mkdir -p ssl_common
+
+if [ "$1" = "done" ]; then
+    cp -f ssl/ca.crt ssl/server.crt ssl/server.key ssl_common/
+else
+    openssl genrsa -out ssl_common/ca.key 2048
+    openssl req -x509 -new -nodes -key ssl_common/ca.key -sha256 -days 1024 -out ssl_common/ca.crt -config ca_config.conf
+    openssl genrsa -out ssl_common/server.key 2048
+    openssl req -new -key ssl_common/server.key -out ssl_common/server.csr -config ca_config.conf
+    openssl x509 -req -days 360 -in ssl_common/server.csr -CA ssl_common/ca.crt -CAkey ssl_common/ca.key -CAcreateserial -out ssl_common/server.crt
+fi
+
+cp -f verifier.dockerfile.template verifier.dockerfile
+
+args=''
+# Use `secret_prov_pf` if base image has encrypted files
+if [ "$2" = "y" ]; then
+    args="--build-arg server_dcap_type=secret_prov_pf"
+fi
+
+# Add encryption key path
+if [ ! -z "$3" ]; then
+    echo 'CMD ["'$3'"]' >> verifier.dockerfile
+fi
+
+cd ..
+cp -f verifier/ssl_common/ca.crt Examples/pytorch/base_image_helper/
+docker build -f verifier/verifier.dockerfile -t verifier $args .
