239236462d99664d2f8c220d505ae72b34c178b7
Kiran Sukhavasi <kiran.sukhavasi@intel.com>
2025-02-06 11:50:26 +0530

    updated pytho3n-venv

--- gsc/test/ovms/commands.sh
+++ gsc/test/ovms/commands.sh
@@ -1,8 +1,8 @@
 curl https://raw.githubusercontent.com/openvinotoolkit/model_server/releases/2023/0/demos/common/python/client_utils.py -o Examples/ovms/client_utils.py
 curl https://raw.githubusercontent.com/openvinotoolkit/model_server/releases/2023/0/demos/face_detection/python/face_detection.py -o Examples/ovms/face_detection.py
 curl --create-dirs https://raw.githubusercontent.com/openvinotoolkit/model_server/releases/2023/0/demos/common/static/images/people/people1.jpeg -o Examples/ovms/images/people1.jpeg
-sudo apt install python3-venv
-pip install -r  Examples/ovms/client_requirements.txt
+sudo apt install -y python3-venv
+pip install -r Examples/ovms/client_requirements.txt
 pip install "numpy<2.0"
 pip3 install opencv-python
 python3 Examples/ovms/face_detection.py --batch_size 1 --width 600 --height 400 --input_images_dir  Examples/ovms/images --output_dir  Examples/ovms/results --grpc_port 9000
