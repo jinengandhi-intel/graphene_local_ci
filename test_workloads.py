bb79ce844356c6cf96f8d333970ee9d58204f177
sswamin1 <shiva.shankari.swaminathan@intel.com>
2025-02-21 14:32:52 +0530

    Pytorch debug debugoptimized addition Docker clean added after each testcase
    Signed-off-by: sswamin1 <shiva.shankari.swaminathan@intel.com>

--- test_workloads.py
+++ test_workloads.py
@@ -380,10 +380,14 @@ class Test_Workload_Results():
         # Test Sequence - Spawn mariadb server in background, run mariadb client, print SUCCESS if successfully launched
         # Check if the string "SUCCESS" is present in and client_output which generated after running the Makefile
         assert "SUCCESS" in open("CI-Examples/mariadb/client_output", "r").read()
-
+       
     @pytest.mark.gsc
     @pytest.mark.skipif(distro_ver != "ubuntu:22.04", reason='GSC pytorch base image version is compatible with Ubuntu 22.04')
     def test_gsc_pytorch_encrypted_workload(self):
+        gsc_build_log = open("gsc_build_log_release", "r")
+        gsc_build_log_content = gsc_build_log.read()
+        assert("RUN cd /gramine     && meson setup build/ --prefix=\"/gramine/meson_build_output\"        --buildtype=release" in gsc_build_log_content)
+        assert("buildtype                                       : release" in gsc_build_log_content)
         gsc_pytorch_verifier_output = open("gsc_pytorch_verifier_result", "r")
         gsc_pytorch_verifier_log = gsc_pytorch_verifier_output.read()
         assert("error: " not in gsc_pytorch_verifier_log)
@@ -394,10 +398,41 @@ class Test_Workload_Results():
         assert("error: " not in gsc_pytorch_log)
 
     @pytest.mark.gsc
+    @pytest.mark.skipif(distro_ver != "ubuntu:22.04", reason='GSC pytorch base image version is compatible with Ubuntu 22.04')
+    def test_gsc_pytorch_encrypted_debug_workload(self):
+        gsc_build_log = open("gsc_build_log_debug", "r")
+        gsc_build_log_content = gsc_build_log.read()
+        assert("RUN cd /gramine     && meson setup build/ --prefix=\"/gramine/meson_build_output\"        --buildtype=debug" in gsc_build_log_content)
+        assert("buildtype                                       : debug" in gsc_build_log_content)
+        gsc_pytorch_verifier_output = open("gsc_pytorch_d_verifier_result", "r")
+        gsc_pytorch_verifier_log = gsc_pytorch_verifier_output.read()
+        assert("error: " not in gsc_pytorch_verifier_log)
+
+        gsc_pytorch_output = open("gsc_pytorch_d_result", "r")
+        gsc_pytorch_log = gsc_pytorch_output.read()
+        assert("Done. The result was written to `result.txt`." in gsc_pytorch_log)
+        assert("error: " not in gsc_pytorch_log)
+
+    @pytest.mark.gsc
+    @pytest.mark.skipif(distro_ver != "ubuntu:22.04", reason='GSC pytorch base image version is compatible with Ubuntu 22.04')
+    def test_gsc_pytorch_encrypted_debugoptimized_workload(self):
+        gsc_build_log = open("gsc_build_log_debugoptimized", "r")
+        gsc_build_log_content = gsc_build_log.read()
+        assert("RUN cd /gramine     && meson setup build/ --prefix=\"/gramine/meson_build_output\"        --buildtype=debugoptimized" in gsc_build_log_content)
+        assert("buildtype                                       : debugoptimized" in gsc_build_log_content)
+        gsc_pytorch_verifier_output = open("gsc_pytorch_do_verifier_result", "r")
+        gsc_pytorch_verifier_log = gsc_pytorch_verifier_output.read()
+        assert("error: " not in gsc_pytorch_verifier_log)
+
+        gsc_pytorch_output = open("gsc_pytorch_do_result", "r")
+        gsc_pytorch_log = gsc_pytorch_output.read()
+        assert("Done. The result was written to `result.txt`." in gsc_pytorch_log)
+        assert("error: " not in gsc_pytorch_log)
+    
+    @pytest.mark.gsc
     @pytest.mark.skipif(distro_ver != "ubuntu:22.04", reason='GSC OVMS image version is compatible with Ubuntu 22.04')
     def test_gsc_ovms_workload(self):
         gsc_ovms_verifier_output = open("gsc_ovms_verifier_result", "r")
         gsc_ovms_verifier_log = gsc_ovms_verifier_output.read()
         assert("error: " not in gsc_ovms_verifier_log)
         assert "SUCCESS" in open("test/ovms/ovms_result.txt", "r").read()
-
